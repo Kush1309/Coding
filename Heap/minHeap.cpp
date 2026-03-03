9951b6b7d91be0297ac53badd2e1b13d975c9324
Kush1309 <kush1309@example.com>
2026-03-04 00:12:49 +0530

    second code

---
+++ Heap/minHeap.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+class minHeap{
+    int *arr;
+    int size;
+    int length;
+
+    public:
+    minHeap(int n){
+        arr = new int[n];
+        size=0;
+        length=n;
+    }
+
+    //insert into heap
+
+    void insert(int value){
+        if(size==length){
+            cout <<"stack overflow";
+            return;
+        }
+        arr[size]=value;
+        int index=size;
+        size++;
+
+    while(length>0 && arr[(index-1)/2]>arr[index]){
+        swap(arr[(index-1)/2],arr[index]);
+        index=(index-1)/2;
+    }
+
+    };
+
+    void print(){
+        for(int i=0;i<size;i++){
+            cout<<arr[i]<<" ";
+        }
+    }
+};
+int main(){
+    minHeap h1(6);
+     h1.insert(45);
+    h1.insert(4);
+    h1.insert(56);
+    h1.insert(42);
+    h1.insert(2);
+    h1.insert(5);
+    h1.print();
+}
