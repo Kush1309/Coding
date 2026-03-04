6a99b95b5f76446e9a102f134c011165322ffabc
Kush1309 <kush1309@example.com>
2026-03-04 23:17:34 +0530

    fourth code

---
+++ Heap/orderNmaxHeap.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//tc o(n)  sc o(log n)
+void heapify(int arr[],int i,int n){
+    int largest=i;
+    int left=2*i+1;
+    int right=2*i+2;
+    if(left<n && arr[left]>arr[largest]){
+        largest=left;
+    }
+    if(right<n && arr[right]>arr[largest]){
+        largest=right;
+    }
+    if(largest!=i){
+        swap(arr[largest],arr[i]);
+        heapify(arr,largest,n);
+    }
+}
+void buildMaxHeap(int arr[],int n){
+    for(int i=(n/2-1);i>=0;i--){
+        heapify(arr,i,n);
+    }
+}
+void print(int arr[],int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" "; 
+    }
+}
+int main(){
+    int n =10;
+    int arr[n]={10,3,8,9,5,13,18,14,11,70};
+    buildMaxHeap(arr,n);
+    print(arr,n);
+}
